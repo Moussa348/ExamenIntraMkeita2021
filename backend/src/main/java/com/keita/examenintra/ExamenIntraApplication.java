892a9f561501defd9adb8fa5c8593dd5dc017727
Moussa <Moussa.Keita2411@gmail.com>
2021-10-19 15:37:15 -0400

    initialization of backend project

---
+++ backend/src/main/java/com/keita/examenintra/ExamenIntraApplication.java
@@ -0,0 +1,13 @@
+package com.keita.examenintra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenIntraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenIntraApplication.class, args);
+    }
+
+}
