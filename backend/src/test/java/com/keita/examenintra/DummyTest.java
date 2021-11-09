0e0cb8e86c3d16bb0441fff2d2d74203e551bd76
Moussa <Moussa.Keita2411@gmail.com>
2021-11-09 00:06:15 -0500

    adding test

---
+++ backend/src/test/java/com/keita/examenintra/DummyTest.java
@@ -0,0 +1,12 @@
+package com.keita.examenintra;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    void test(){
+        Assertions.assertTrue(true);
+    }
+}
